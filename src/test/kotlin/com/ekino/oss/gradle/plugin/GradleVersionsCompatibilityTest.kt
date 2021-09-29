bb2c18d8751aea042331e254c4509792cfbb1709
Clement Stoquart <clement.stoquart@ekino.com>
2021-09-29 13:47:43 +0200

    chore: upgrade gradle to 7.2

--- src/test/kotlin/com/ekino/oss/gradle/plugin/GradleVersionsCompatibilityTest.kt
+++ src/test/kotlin/com/ekino/oss/gradle/plugin/GradleVersionsCompatibilityTest.kt
@@ -15,7 +15,7 @@ class GradleVersionsCompatibilityTest {
     @TempDir
     lateinit var tempDir: File
 
-    @ValueSource(strings = ["6.8.3", "6.9", "7.1.1"])
+    @ValueSource(strings = ["6.8.3", "6.9", "7.1.1", "7.2"])
     @ParameterizedTest(name = "Gradle {0}")
     @DisplayName("Should work in Gradle version")
     fun shouldWorkInGradleVersion(gradleVersion: String) {
